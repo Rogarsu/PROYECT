77f793e464bc5fba89916a59f28155733dffdd67
Rogarsu <robertogarciasud11@gmail.com>
2022-07-03 01:52:37 -0500

    commit2

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/*.html'],
+  theme: {
+    colors:{
+      'first':'#F5F0BB',
+      'second':'#C4DFAA',
+      'tird':'#90C8AC',
+      'forth':'#73A9AD'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
